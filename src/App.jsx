6eb89242fd3f65dbe3c8e933b69873997b7bfc8e
Alin Nita <mr.alin.nita@gmail.com>
2021-12-06 15:55:04 +0000

    created components

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <h1>API Beers</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
